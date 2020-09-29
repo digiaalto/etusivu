abf3481d3c64da25892cc29402a937b7447b986a
leevinikkila <nikkilaleevi@gmail.com>
2020-09-30 01:38:00 +0300

    added noindex for robots

--- components/layout.js
+++ components/layout.js
@@ -11,6 +11,8 @@ const Layout = ({ title, children }) => {
 				<link rel="icon" href="/favicon.ico" />
 				<meta charSet="UTF-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<meta name="robots" content="noindex,follow">
+				<meta name="googlebot" content="noindex,follow">
 				<title>{title}</title>
 			</Head>
 			<Navigation />
