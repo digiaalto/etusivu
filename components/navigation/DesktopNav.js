43407af3bca53922ccbde152a7b3571daa2e96c7
Leevi Nikkilä <nikkilaleevi@gmail.com>
2020-10-23 22:01:48 +0300

    font changes

--- components/navigation/DesktopNav.js
+++ components/navigation/DesktopNav.js
@@ -1,13 +1,23 @@
 import styles from "./DesktopNav.module.sass"
+import React from "react"
+import Link from "next/link"
+
+const ForwardLink = React.forwardRef(({ activateItem, text, href }, ref) => {
+  return (
+    <a href={href} onClick={() => activateItem(href)} ref={ref}>
+      {text}
+    </a>
+  )
+})
 
 const DesktopNav = ({ items, activateItem }) => {
   return (
     <ul className={styles.navList}>
       {items.map((linkObject, index) => (
         <li className={styles.navItem} key={`desktop-nav-item-${index}`}>
-          <span onClick={() => activateItem(linkObject.href)}>
-            {linkObject.text}
-          </span>
+          <Link href={linkObject.href} passHref>
+            <ForwardLink activateItem={activateItem} text={linkObject.text} />
+          </Link>
         </li>
       ))}
     </ul>
