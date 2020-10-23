0779b1a08daa71a5f2f7dcfda49bc1f442e32506
Leevi Nikkilä <nikkilaleevi@gmail.com>
2020-10-23 22:45:13 +0300

    fixed desktop link bug

--- components/navigation/DesktopNav.js
+++ components/navigation/DesktopNav.js
@@ -1,22 +1,13 @@
 import styles from "./DesktopNav.module.sass"
-import React from "react"
 import Link from "next/link"
-
-const ForwardLink = React.forwardRef(({ activateItem, text, href }, ref) => {
-  return (
-    <a href={href} onClick={() => activateItem(href)} ref={ref}>
-      {text}
-    </a>
-  )
-})
 
 const DesktopNav = ({ items, activateItem }) => {
   return (
     <ul className={styles.navList}>
       {items.map((linkObject, index) => (
         <li className={styles.navItem} key={`desktop-nav-item-${index}`}>
-          <Link href={linkObject.href} passHref>
-            <ForwardLink activateItem={activateItem} text={linkObject.text} />
+          <Link href={linkObject.href}>
+            <a>{linkObject.text}</a>
           </Link>
         </li>
       ))}
