a5019fc80e2d821e64dbc289b33580e982a7c2d2
Leevi Nikkilä <nikkilaleevi@gmail.com>
2020-10-27 13:49:04 +0200

    content tweak

--- components/LP_Audit.js
+++ components/LP_Audit.js
@@ -7,14 +7,14 @@ const LP_Audit = () => {
     <section className={styles.section}>
       <div className={styles.content}>
         <span className={styles.preheader}>
-          Vertaile kilpailijasi sivuja omiisi
+          Vertaile kilpailijan sivuja omiisi
         </span>
-        <span className={styles.suggestion}>Auditoi verkkosivun laatu.</span>
+        <span className={styles.suggestion}>Onkohan parantamisen varaa?</span>
         <p className={styles.info}>
-          Google Lighthouse on avoimen automaattinen työkalu verkkosivujen
-          laadun mittaamiseen. Sitä voidaan käyttää minkä tahansa julkisen
-          verkkosivun kanssa. Google Lighthouse tarkastaa verkkosivujen
-          suorituskyvyn, saavutettavuuden ja hakukoneoptimoinnin.
+          Google Lighthouse on automaattinen työkalu verkkosivujen laadun
+          mittaamiseen josta selviää kuinka verkkosivusi näkyy ja pärjää
+          verkossa. Sillä voidaan auditoida mikä tahansa verkkosivun
+          suorituskyky, saavutettavuus ja hakukoneoptimointi.
         </p>
         <a
           className={styles.button}
@@ -22,7 +22,7 @@ const LP_Audit = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Kokeile testiä
+          Kokeile nopeaa testiä
         </a>
         <p className={styles.help}>
           <HiExclamation className={styles.helpIcon} /> Tulokset ovat
