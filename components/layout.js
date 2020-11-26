e585debe78420fc43f26174c2d70c5284a31e2ed
Leevi Nikkilä <nikkilaleevi@gmail.com>
2020-11-26 17:14:02 +0200

    some optimization

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,18 @@
 import React from "react"
-import PropTypes from "prop-types"
 import Head from "next/head"
 import DesktopNavigation from "./NavDesktop"
 
-const Layout = ({ title, children, infobar = null }) => {
+const Layout = ({ title, type, description, children, infobar = null }) => {
+  const SEO = {
+    title: title
+      ? `${title} | Digiaalto.fi`
+      : `Optimoidut verkkosivut nettiin vaivattomasti | Digiaalto.fi`, // 60
+    type: type ? type : "website",
+    description: description
+      ? description
+      : `Digiaalto rakentaa laatutestattuja verkkosivuja kiireiselle yrittäjälle. Modernilla tekniikalla optimoitu verkkokehitys ja suunnittelupalvelu. Avaimet käteen.`,
+    image: "/brand/some-image.png",
+  }
   return (
     <React.Fragment>
       <Head>
@@ -12,26 +21,19 @@ const Layout = ({ title, children, infobar = null }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="robots" content="noindex,follow" />
         <meta name="googlebot" content="noindex,follow" />
-        <title>{title}</title>
+
+        <title>{SEO.title}</title>
+        <meta name="description" content={SEO.description} />
+        <meta name="og:url" content="https://www.digiaalto.fi" />
+        <meta name="og:type" content={SEO.type} />
+        <meta name="og:title" content={SEO.title} />
+        <meta name="og:image" content={SEO.image} />
       </Head>
       {infobar}
       <DesktopNavigation />
-      {/* <Nav /> */}
       <main>{children}</main>
     </React.Fragment>
   )
-}
-
-Layout.defaultProps = {
-  title: "Verkkosivut jotka löytyvät | Digiaalto.fi",
-}
-
-Layout.propTypes = {
-  title: PropTypes.string.isRequired,
-  children: PropTypes.oneOfType([
-    PropTypes.object.isRequired,
-    PropTypes.arrayOf(PropTypes.object).isRequired,
-  ]),
 }
 
 export default Layout
