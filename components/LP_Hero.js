12e4fe090dc5387fc7438cfa257aec8a354e4625
Leevi Nikkilä <nikkilaleevi@gmail.com>
2020-12-17 23:36:12 +0200

    hero reworking, content changes

--- components/LP_Hero.js
+++ components/LP_Hero.js
@@ -1,18 +1,69 @@
 import styles from "../styles/LP_Hero.module.sass"
+import React, { useState, useEffect } from "react"
 import Link from "next/link"
 
 const Hero = () => {
+  const [animElement, setAnimElement] = useState(0)
+
+  useEffect(() => {
+    const animInterval = setInterval(() => {
+      let animElementIndex = animElement + 1
+      if (animElementIndex > 2) {
+        animElementIndex = 0
+      }
+      console.log(`setting animElement: ${animElementIndex}`)
+      setAnimElement(animElementIndex)
+    }, 2500)
+    return () => clearInterval(animInterval)
+  })
+
   return (
     <section className={styles.section}>
       <div className={styles.content}>
-        <span className={styles.salesPitch}>
-          Haluatko <span className={styles.highlight}> löytyä </span>
-          verkosta <span className={styles.highlight2}> helposti </span>
-          sekä <span className={styles.highlight3}> nopeasti?</span>
-        </span>
-        <h1 className={styles.header}>
-          Verkkokehittäjä Digiaalto on erikoistunut suunnittelemaan
-          laatutestattuja verkkosivuja yrittäjälle kenen kalenteri on täynnä.
+        <h1 className={styles.salesPitch}>
+          <span
+            className={`${styles.highlightBackground} ${
+              animElement === 0 ? styles.reverseFadeAnimation : null
+            }`}
+          >
+            <span
+              className={`${styles.highlightForeground} ${
+                animElement === 0 ? styles.fadeAnimation : null
+              }`}
+            >
+              Suunnittelu.
+            </span>
+          </span>
+          <div
+            className={`${styles.highlightBackground2} ${
+              animElement === 1 ? styles.reverseFadeAnimation : null
+            }`}
+          >
+            <span
+              className={`${styles.highlightForeground2} ${
+                animElement === 1 ? styles.fadeAnimation : null
+              }`}
+            >
+              Kehitys.
+            </span>
+          </div>
+          <div
+            className={`${styles.highlightBackground3} ${
+              animElement === 2 ? styles.reverseFadeAnimation : null
+            }`}
+          >
+            <span
+              className={`${styles.highlightForeground3} ${
+                animElement === 2 ? styles.fadeAnimation : null
+              }`}
+            >
+              Julkaisu.
+            </span>
+          </div>
+        </h1>
+        <h1 className={styles.subheader}>
+          Laatutestattuja yrityssivuja vaivattomasti kiireisille Suomalaisille
+          yrittäjille.
         </h1>
         <Link href="#kehityksen-haasteita">
           <a>
