d6106eeddea14c751177b5be980c9299bd1b9d02
Leevi Nikkilä <nikkilaleevi@gmail.com>
2021-02-01 00:32:35 +0200

    Removed more icons from landing page and tweaked content.

--- components/etusivu/Ominaisuudet.js
+++ components/etusivu/Ominaisuudet.js
@@ -1,27 +1,21 @@
 import styles from "./Ominaisuudet.module.sass"
 import React from "react"
-import { SiPowerbi } from "react-icons/si"
-import { FaCog } from "react-icons/fa"
-import { FaPaintBrush } from "react-icons/fa"
 
 const ominaisuudet = [
   {
     header: "Laatutestattu",
     text:
-      "Pidämme huolen että verkkosivumme ovat optimoitu ja tehostettu maailman suosituimman verkkosivujen laatutestin mukaisesti. Sivustoja ei julkaista ennen kun jokainen epäonnistumisen indikaattori on hiottu kuntoon.",
-    icon: <SiPowerbi className={styles.growing} />,
+      "Kutsumuksemme on valmistaa tarkasti optimoituja, tehokkaita verkkosivuja, jotka erottuvat omaan luokkaansa maailman suosituimman verkkosivujen laatutestin sadoissa eri testeissä.",
   },
   {
     header: "Ongelmaton",
     text:
       "Uskomme että verkkosivujen kuuluu toimia itsenäisesti ja pitkäikäisesti julkaisun jälkeenkin. Sivustomme rakentavat ja uudelleenrakentavat itseään regeneroimalla staattiset osansa ja minimoimalla hyökkäyspinnan.",
-    icon: <FaCog className={styles.spinning} />,
   },
   {
     header: "Suunniteltu",
     text:
-      "Luomme yksilöityjä kotisivuja joilla tuomme yrityksesi persoonan ja vahvuudet esille. Suunnittelussa olemme mahdollisimman asiakaslähtöisiä, sivustomme toimivat käyttäjäystävällisesti ja herättävät luottamusta.",
-    icon: <FaPaintBrush className={styles.bouncing} />,
+      "Luomme yksilöityjä kotisivuja joilla tuomme yrityksesi persoonan ja vahvuudet esille. Suunnittelussa olemme mahdollisimman asiakaslähtöisiä, sivustomme ovat käyttäjäystävällisiä ja luottamusta herättäviä.",
   },
 ]
 
@@ -36,10 +30,9 @@ const Ominaisuudet = () => {
 }
 
 const Ominaisuus = (props) => {
-  const { icon, header, text } = props.data
+  const { header, text } = props.data
   return (
     <div className={styles.ominaisuus}>
-      <div className={styles.iconWrapper}>{icon}</div>
       <span className={styles.header}>{header}</span>
       <p className={styles.textWrap}>{text}</p>
     </div>
